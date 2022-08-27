42803950ba2196aceb7962b45ba1ee3f329132bf
Rorical <46294886+Rorical@users.noreply.github.com>
2022-08-27 19:58:51 +0800

    update binocular cam

--- experiments/binocular_cam/binocular_camera.py
+++ experiments/binocular_cam/binocular_camera.py
@@ -18,7 +18,6 @@ def display(frame):
     image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     plt.imshow(image)
     plt.show()
-
 # 视差计算
 def stereoMatchSGBM(left_image, right_image, down_scale=False):
     # SGBM匹配参数设置
@@ -42,33 +41,24 @@ def stereoMatchSGBM(left_image, right_image, down_scale=False):
  
     # 构建SGBM对象
     left_matcher = cv2.StereoSGBM_create(**paraml)
-    paramr = paraml
-    paramr['minDisparity'] = -paraml['numDisparities']
-    right_matcher = cv2.StereoSGBM_create(**paramr)
  
     # 计算视差图
     size = (left_image.shape[1], left_image.shape[0])
     if down_scale == False:
         disparity_left = left_matcher.compute(left_image, right_image)
-        disparity_right = right_matcher.compute(right_image, left_image)
  
     else:
         left_image_down = cv2.pyrDown(left_image)
         right_image_down = cv2.pyrDown(right_image)
         factor = left_image.shape[1] / left_image_down.shape[1]
  
-        disparity_left_half = left_matcher.compute(left_image_down, right_image_down)
-        disparity_right_half = right_matcher.compute(right_image_down, left_image_down)
-        disparity_left = cv2.resize(disparity_left_half, size, interpolation=cv2.INTER_AREA)
-        disparity_right = cv2.resize(disparity_right_half, size, interpolation=cv2.INTER_AREA)
+        disparity_left = left_matcher.compute(left_image_down, right_image_down)
         disparity_left = factor * disparity_left
-        disparity_right = factor * disparity_right
  
     # 真实视差（因为SGBM算法得到的视差是×16的）
     trueDisp_left = disparity_left.astype(np.float32) / 16.
-    trueDisp_right = disparity_right.astype(np.float32) / 16.
  
-    return trueDisp_left, trueDisp_right
+    return trueDisp_left
 
 def getDepthMapWithQ(disparityMap : np.ndarray, Q : np.ndarray) -> np.ndarray:
     points_3d = cv2.reprojectImageTo3D(disparityMap, Q)
@@ -78,18 +68,41 @@ def getDepthMapWithQ(disparityMap : np.ndarray, Q : np.ndarray) -> np.ndarray:
  
     return depthMap.astype(np.float32)
 
+
+def rectifyImage(image1, image2):
+    rectifyed_img1 = cv2.undistort(image1, camera_config.left_camera_matrix, camera_config.left_distortion)
+    rectifyed_img2 = cv2.undistort(image2, camera_config.right_camera_matrix, camera_config.right_distortion)
+    
+    rectifyed_img1 = cv2.remap(rectifyed_img1, camera_config.left_map1, camera_config.left_map2, cv2.INTER_AREA)
+    rectifyed_img2 = cv2.remap(rectifyed_img2, camera_config.right_map1, camera_config.right_map2, cv2.INTER_AREA)
+ 
+    return rectifyed_img1, rectifyed_img2
+
 while cap.isOpened():
     ret, frame = cap.read()
     left_img = frame[:, 0:1280]
     right_img = frame[:, 1280:2560]
+
+    left_img = cv2.medianBlur(left_img, 7)
+    right_img = cv2.medianBlur(right_img, 7)
     
-    left_disp, right_disp = stereoMatchSGBM(left_img, right_img)
+    left_img = cv2.cvtColor(left_img, cv2.COLOR_BGR2GRAY)
+    right_img = cv2.cvtColor(right_img, cv2.COLOR_BGR2GRAY)
     
-    left_disp = cv2.bilateralFilter(left_disp,7,10,10)
+    left_img = cv2.equalizeHist(left_img)
+    right_img = cv2.equalizeHist(right_img)
     
-    left_disp = cv2.morphologyEx(left_disp, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT,(3,3)), iterations=3)
+    left_img, right_img = rectifyImage(left_img, right_img)
+        
+    left_disp = stereoMatchSGBM(left_img, right_img, down_scale= True)
     
-    #left_depth = getDepthMapWithQ(left_disp, camera_config.Q)
+    left_disp = cv2.medianBlur(left_disp, 5)
+    
+    left_disp = cv2.morphologyEx(left_disp, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT,(5,5)), iterations=3)
+        
+    left_disp = left_disp[:, 128:]
+    
+    # 显示点云
     #right_depth = getDepthMapWithQ(right_disp, camera_config.Q)
     
     #plt.imshow(cv2.resize(left_disp, (22, 8), interpolation=cv2.INTER_AREA))
